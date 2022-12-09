1e22f509506c2e1c324b783c37ea27f54808dad9
LouisOkwaro <okwarolouis@gmail.comgit config --global user.email okwarolouis@gmail.com>
2022-12-09 18:45:21 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
 
+  // Write your algorithm here
+  for (let i = 0; i < word.length / 2; i++) {
+
+    const j = word.length - 1 - i;
+
+    if (word[i] !== word[j]) {
+      // if the letters don't match, return false
+      return false;
+    }
+}
+ // if we reach the middle, and all the letters match, return true
+ return true;
+}
 /* 
-  Add your pseudocode here
+ 
 */
 
 /*
